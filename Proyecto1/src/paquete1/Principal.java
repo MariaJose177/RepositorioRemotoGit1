f28b7d08d0f427a06881ed74b42ee19e911eaa16
Mjromero <Mjromero@192.168.1.151>
2026-01-28 09:07:15 +0100

    rama2

--- Proyecto1/src/paquete1/Principal.java
+++ Proyecto1/src/paquete1/Principal.java
@@ -4,7 +4,7 @@ public class Principal {
 
 	public static void main(String[] args) {
 		System.out.println ("Estamos haciendo control de versiones con Git 5");
-
+		System.out.println ("rama2");
 	}
 
 }
